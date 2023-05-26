b84186575aeb4dc4cb9c4261b3f25e4439217cde
guohongliang <guohongliang@163.com>
2023-05-26 23:17:26 +0800

    微信手机端测试搭建

---
+++ src/wework/contact/search_page.py
@@ -0,0 +1,4 @@
+# 姓名：郭宏亮
+# 时间：2023/5/26 22:42
+class SearchPage:
+    pass
---
+++ src/wework/home_page.py
@@ -0,0 +1,21 @@
+# 姓名：郭宏亮
+# 时间：2023/5/26 22:39
+from src.wework.contact.search_page import SearchPage
+
+
+class HomePage:
+
+    def 消息(self):
+        pass
+
+    def 邮件(self):
+        pass
+
+    def 文档(self):
+        pass
+
+    def 工作台(self):
+        pass
+
+    def 通讯录(self):
+        return SearchPage()
